5b69fda588ac11dde9581685ce1646072449334f
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-20 21:07:16 -0600

    Agregar rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Ng2Webstorage } from 'ngx-webstorage';
 
 
 import { AppComponent } from './app.component';
@@ -11,6 +14,12 @@ import { FooterComponent } from './common/footer/footer.component';
 import { LoginComponent } from './public/login/login.component';
 import { HomeComponent } from './public/home/home.component';
 import { BookmarksComponent } from './auth/bookmarks/bookmarks.component';
+import { NotFoundComponent } from './common/not-found/not-found.component';
+import { routes } from './routes';
+import { HttpService } from './common/services/http.service';
+import { AuthGuard } from './common/guards/auth.guard';
+import { PublicGuard } from './common/guards/public.guard';
+import { AuthenticationService } from './common/services/authentication.service';
 
 
 @NgModule({
@@ -20,19 +29,30 @@ import { BookmarksComponent } from './auth/bookmarks/bookmarks.component';
     FooterComponent,
     LoginComponent,
     HomeComponent,
-    BookmarksComponent
+    BookmarksComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     // Include it under 'imports' in your application module
     // after BrowserModule.
     HttpClientModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
     // Required by Angular Material
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    // Auth
+    Ng2Webstorage
   ],
-  providers: [],
+  providers: [
+    HttpService,
+    PublicGuard, AuthGuard, // Guards
+    AuthenticationService
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
