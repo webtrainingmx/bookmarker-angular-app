b8e094d615b5244fd30414cdb17dc5017482990f
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-20 19:34:33 -0600

    Agregar componentes base
    ng generate component common/header
    ng generate component common/footer
    ng generate component public/login
    ng generate component public/home
    ng generate component auth/bookmarks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { LoginComponent } from './public/login/login.component';
+import { HomeComponent } from './public/home/home.component';
+import { BookmarksComponent } from './auth/bookmarks/bookmarks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    BookmarksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Include it under 'imports' in your application module
+    // after BrowserModule.
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
