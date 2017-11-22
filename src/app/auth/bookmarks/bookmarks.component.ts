3dfd400d59b942c35eaa616632675c07bea4ede9
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-21 18:42:12 -0600

    Agregar bookmarks component
    Incluye template y SCSS

--- src/app/auth/bookmarks/bookmarks.component.ts
+++ src/app/auth/bookmarks/bookmarks.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Bookmark } from './models/bookmark.model';
+import { BookmarksService } from './services/bookmarks.service';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
-  styleUrls: ['./bookmarks.component.scss']
+  styleUrls: [ './bookmarks.component.scss' ]
 })
 export class BookmarksComponent implements OnInit {
 
-  constructor() { }
+  bookmarks: Array<Bookmark>;
+  displayedColumns = [ 'id', 'title', 'description', 'created' ];
+  dataSource: MatTableDataSource<Bookmark>;
 
-  ngOnInit() {
+  constructor (private _bookmarksService: BookmarksService) {
+  }
+
+  applyFilter (filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
+  ngOnInit () {
+    this._bookmarksService.getAll().subscribe(
+      (data: Array<Bookmark>) => {
+        this.bookmarks = data;
+        this.dataSource = new MatTableDataSource<Bookmark>(this.bookmarks);
+      });
   }
 
 }
