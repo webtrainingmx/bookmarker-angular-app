1259d868f1f340f3bae0e0bf2821bf2bf46f78b0
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-11-24 19:20:45 -0600

    Agregar URL servidor de Japón

--- src/app/common/services/http.service.ts
+++ src/app/common/services/http.service.ts
@@ -11,7 +11,8 @@ export class HttpService {
 
   constructor (protected _http: HttpClient) {
     this.apiAuthBaseURL = 'http://projects-api.webtraining.zone';
-    this.apiBookmarksURL = 'http://localhost:8765'; // 'http://localhost:8080/bookmarker/api/v1';
+    this.apiBookmarksURL = 'http://bookmarks-api-cakephp.webtraining.zone';
+    // 'http://localhost:8765'; // 'http://localhost:8080/bookmarker/api/v1';
   }
 
   public get (url, token): Observable<any> {
